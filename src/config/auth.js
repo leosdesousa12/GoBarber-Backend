e86e82c143cfedfbdc102347459e64f6a09cd60c
leosdesousa <leonardo.silva@academico.ifpb.edu.br>
2020-04-11 19:23:43 -0300

    Implementing JWT Authentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e536521c8e62c07958470e354e3bf8eb',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 
+import SessionControler from './app/controllers/SessionController';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionControler.store);
 
 export default routes;
