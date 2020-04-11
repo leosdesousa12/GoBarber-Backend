bedb1275e86f791a854b2596e18909badd4629c8
leosdesousa <leonardo.silva@academico.ifpb.edu.br>
2020-04-11 01:41:14 -0300

    loader models created user and testing create user

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
-routes.get('/', (req, res) => res.json({ message: 'hello world' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'leonaseo silva',
+    email: 'leosdesousa@gmail.com',
+    password_hash: '15135153135148',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
