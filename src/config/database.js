813fe186b366b74bb0dbd5ae2594d6b148724d77
leosdesousa <leonardo.silva@academico.ifpb.edu.br>
2020-04-11 00:38:50 -0300

    Configure Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'root123',
+  database: 'goBarberBD',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
