61d9f3e9494ebcaad7b9ddb0a4f47a071d02f0cb
leosdesousa <leonardo.silva@academico.ifpb.edu.br>
2020-04-12 19:45:33 -0300

    Middleware authentication

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,14 @@ import UserController from './app/controllers/UserController';
 
 import SessionControler from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionControler.store);
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
